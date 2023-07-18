d4893f2744be91cc1ca04c6c668b18285f5958ab
Hailemichael Atrsaw <hailat.alx@gmail.com>
2023-07-18 15:59:45 +0300

    DOckerfile env value changed from Allowed hosts to url

--- src/core/settings.py
+++ src/core/settings.py
@@ -106,8 +106,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 MEDIA_URL = 'Media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
