9de92af9a25d145c2193a5b9a71b347f43056fc0
Hailemichael Atrsaw <hailat.alx@gmail.com>
2023-08-15 17:39:45 +0300

    tasks that I remember completed

--- src/vote/serializers.py
+++ src/vote/serializers.py
@@ -12,13 +12,17 @@ class VoteSerializer(serializers.ModelSerializer):
         ipv4 = validated_data.get('ipv4')
         finger_print = validated_data.get('finger_print')
         
-        if VoteModel.objects.filter(category=category, ipv4=ipv4, finger_print=finger_print).exists():
+        if VoteModel.objects.filter(category=category, ipv4=ipv4).exists():
+            raise serializers.ValidationError('You can only vote once per category!')
+        
+        if VoteModel.objects.filter(category=category, finger_print=finger_print).exists():
             raise serializers.ValidationError('You can only vote once per category!')
         
         instance = super().create(validated_data)
         return instance
         
 class CandidateSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = CandidateModel
         fields = '__all__'
@@ -26,7 +30,30 @@ class CandidateSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
     candidates = CandidateSerializer(many=True, read_only=True)
+    category_vote_count = serializers.SerializerMethodField()
+    vote_percentage = serializers.SerializerMethodField()
     
     class Meta:
         model = CategoryModel
         fields = '__all__'
+    
+    def get_category_vote_count(self, instance):
+        return VoteModel.objects.filter(category=instance.id).count()
+        
+    def get_vote_percentage(self, instance):
+        total_vote_count = VoteModel.objects.all().count()
+        category_vote_count = self.get_category_vote_count(instance)
+        percentage = (category_vote_count / total_vote_count) * 100 if total_vote_count else 0
+        return percentage
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        category_id = representation['id']
+        for candidate_data in representation['candidates']:
+            category_query = VoteModel.objects.filter(category=category_id)
+            total_category_vote_count = category_query.count()
+            candidate_vote_count = category_query.filter(category=category_id, candidate=candidate_data['id']).count()
+            candidate_data['vote_count_in_the_category'] = candidate_vote_count
+            candidate_data['vote_percentage_in_the_category'] = (candidate_vote_count / total_category_vote_count) * 100 if total_category_vote_count else 0
+                
+        return representation
--- src/vote/views.py
+++ src/vote/views.py
@@ -1,4 +1,7 @@
+from django.db.models import Q
+from datetime import date, timedelta
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.status import HTTP_405_METHOD_NOT_ALLOWED
@@ -12,6 +15,15 @@ class CategoryViewSet(ModelViewSet):
     queryset = CategoryModel.objects.all().order_by('id')
     serializer_class = CategorySerializer
     permission_classes=[CustomPermission]
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        fetch_top_categories = self.request.query_params.get('fetch_top_categories')
+
+        if fetch_top_categories:
+            queryset = queryset.filter(is_top_category=True)
+
+        return queryset
     
     def create(self, request, *args, **kwargs):
         if not request.user.has_perm('vote.add_categorymodel'):
@@ -32,8 +44,7 @@ class CategoryViewSet(ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         if not request.user.has_perm('vote.delete_categorymodel'):
             return Response({'message': 'You do not have permission to access this resource.'}, status=403)
-        return super().destroy(request, *args, **kwargs)
-        
+        return super().destroy(request, *args, **kwargs)        
 
 class CandidateViewSet(ModelViewSet):
     queryset = CandidateModel.objects.all().order_by('id')
@@ -61,33 +72,50 @@ class CandidateViewSet(ModelViewSet):
             return Response({'message': 'You do not have permission to access this resource.'}, status=403)
         return super().destroy(request, *args, **kwargs)
 
-
 class VoteViewSet(ModelViewSet):
     queryset = VoteModel.objects.all().order_by('id')
     serializer_class = VoteSerializer
     
-    def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
-    
     def update(self, request, *args, **kwargs):
        raise MethodNotAllowed(request.method, HTTP_405_METHOD_NOT_ALLOWED)
 
-    
     def retrieve(self, request, *args, **kwargs):
         raise MethodNotAllowed(request.method, HTTP_405_METHOD_NOT_ALLOWED)
-
     
     def list(self, request, *args, **kwargs):
-        queryset = CategoryModel.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
+        response = {}
         
-        for category in serializer.data:
-            for candidate in category['candidates']:
-                candidate['percentage'] = (self.queryset.filter(category=category['id'], candidate=candidate['id']).count() / len(category['candidates'])) * 100
-        return Response(serializer.data)
-    
-    # def list(self, request, *args, **kwargs):
-    #     return super().list(request, *args, **kwargs)
+        category_id = self.request.query_params.get('category_id')
+        candidate_id = self.request.query_params.get('candidate_id')
+        
+        today = date.today()
+        
+        start_of_week = today - timedelta(days=today.weekday())
+        end_of_week = start_of_week + timedelta(days=6)
+        
+        start_of_month = date(today.year, today.month, 1)
+        end_of_month = date(today.year, today.month + 1, 1) - timedelta(days=1)
+        
+       
+        if category_id:
+            category_vote = self.queryset.filter(category=int(category_id))
+            response["category_votes_of_the_day"] = category_vote.filter(created_at__date=today).count()
+            response["category_votes_of_the_week"] = category_vote.filter(created_at__date__range=[start_of_week, end_of_week]).count()
+            response["category_votes_of_the_month"] = category_vote.filter(created_at__date__range=[start_of_month, end_of_month]).count()
+            return Response(response, status=status.HTTP_200_OK)
+        
+        if candidate_id:
+            candidate_vote = self.queryset.filter(candidate=int(candidate_id))
+            response["candidate_votes_of_the_day"] = candidate_vote.filter(created_at__date=today).count()
+            response["candidate_votes_of_the_week"] = candidate_vote.filter(created_at__date__range=[start_of_week, end_of_week]).count()
+            response["candidate_votes_of_the_month"] = candidate_vote.filter(created_at__date__range=[start_of_month, end_of_month]).count()
+            return Response(response, status=status.HTTP_200_OK)
+        
+        response["total_votes_of_the_day"] = self.queryset.filter(created_at__date=today).count()
+        response["total_votes_of_the_week"] = self.queryset.filter(created_at__date__range=[start_of_week, end_of_week]).count()
+        response["total_votes_of_the_month"] = self.queryset.filter(created_at__date__range=[start_of_month, end_of_month]).count()
+
+        return Response(response, status=status.HTTP_200_OK)
     
     def destroy(self, request, *args, **kwargs):
         raise MethodNotAllowed(request.method, HTTP_405_METHOD_NOT_ALLOWED)
